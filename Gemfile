8990dcf6b7dafa74b0d34f3d2d4147c6bdcc7d9e
kuma-900 <129268970+kuma-900@users.noreply.github.com>
2024-11-13 22:41:44 +0900

    メモアプリ初回コミット

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# gem "rails"
+gem 'erb_lint', require: false
+
+gem 'sinatra'
+gem 'webrick'
+gem 'rackup', '~> 2.1'
+
+gem 'json'
+
+group :development do
+  gem 'rubocop-fjord', require: false
+end
---
+++ app.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'json'
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+def read_memos
+  JSON.parse(File.read('data/memos.json'))
+end
+
+def save_memos(memos)
+  File.write('data/memos.json', JSON.pretty_generate(memos))
+end
+
+get '/memos' do
+  @memos = read_memos
+  erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos' do
+  memos = read_memos
+  new_memo = {
+    'id' => memos.empty? ? 1 : memos.last['id'] + 1,
+    'title' => params[:title],
+    'content' => params[:content]
+  }
+  memos << new_memo
+  save_memos(memos)
+  redirect '/memos'
+end
+
+get '/memos/:memo_id' do
+  @memo = read_memos.find { |memo| memo['id'] == params[:memo_id].to_i }
+  halt 404, 'メモが見つかりません' unless @memo
+  erb :show
+end
+
+get '/memos/:memo_id/edit' do
+  @memo = read_memos.find { |memo| memo['id'] == params[:memo_id].to_i }
+  halt 404, 'メモが見つかりません' unless @memo
+  erb :edit
+end
+
+patch '/memos/:memo_id' do
+  memos = read_memos
+  memo = memos.find { |m| m['id'] == params[:memo_id].to_i }
+  halt 404, 'メモが見つかりません' unless memo
+
+  memo['title'] = params[:title]
+  memo['content'] = params[:content]
+  save_memos(memos)
+  redirect "/memos/#{params[:memo_id]}"
+end
+
+delete '/memos/:memo_id' do
+  memos = read_memos
+  memos.reject! { |memo| memo['id'] == params[:memo_id].to_i }
+  save_memos(memos)
+  redirect '/memos'
+end
